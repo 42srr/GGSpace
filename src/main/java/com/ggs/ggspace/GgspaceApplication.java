57bbf41591a6aaf323be74c29a537c4dc3928249
joonwan <joonwan1024@naver.com>
2025-03-06 21:12:54 +0900

    feat: init project

---
+++ src/main/java/com/ggs/ggspace/GgspaceApplication.java
@@ -0,0 +1,13 @@
+package com.ggs.ggspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggs/ggspace/GgspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggs.ggspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
