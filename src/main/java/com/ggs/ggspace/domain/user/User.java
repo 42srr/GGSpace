d89c73908404fc9c0346a685b0c9add141e10ef1
joonwan <joonwan1024@naver.com>
2025-03-07 23:34:27 +0900

    fix: User Entity intraId 필드 unique 제약조건 적용

--- src/main/java/com/ggs/ggspace/domain/user/User.java
+++ src/main/java/com/ggs/ggspace/domain/user/User.java
@@ -15,7 +15,7 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 20)
+    @Column(length = 20, unique = true)
     private String intraId;
 
     private String img;
