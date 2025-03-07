68c55ccf67c9cbafdbb206e5877956b8ebd05479
joonwan <joonwan1024@naver.com>
2025-03-07 22:53:57 +0900

    feat: User Entity 개발

---
+++ src/main/java/com/ggs/ggspace/domain/user/User.java
@@ -0,0 +1,29 @@
+package com.ggs.ggspace.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20)
+    private String intraId;
+
+    private String img;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Builder
+    private User(String intraId, String img, Role role) {
+        this.intraId = intraId;
+        this.img = img;
+        this.role = role;
+    }
+}
