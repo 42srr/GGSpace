f8a1171eff324c10dfde1ed8932f0dd6d866f648
조재영 <whwodud0303@naver.com>
2025-03-21 22:22:49 +0900

    feat: study 엔티티 개발

---
+++ src/main/java/com/ggs/ggspace/domain/study/Study.java
@@ -0,0 +1,36 @@
+package com.ggs.ggspace.domain.study;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.security.Timestamp;
+
+@Entity
+@Table(name = "study")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Study {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20)
+    private String name;
+
+    @Column(length = 3000)
+    private String description;
+
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    @Builder
+    public Study(String name, String description, Timestamp createdAt, Timestamp updatedAt) {
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
