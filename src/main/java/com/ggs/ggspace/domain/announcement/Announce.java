698192324eb13c07b1fe9dfbb7bf9578f9e305fa
조재영 <whwodud0303@naver.com>
2025-03-21 21:40:23 +0900

    feat: Announce 엔티티 개발

---
+++ src/main/java/com/ggs/ggspace/domain/announcement/Announce.java
@@ -0,0 +1,24 @@
+package com.ggs.ggspace.domain.announcement;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Table(name = "announce")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Announce {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 3000)
+    private String contents;
+
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
