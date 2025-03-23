841c6601bc52a8a22c07f276979b72cb0a7f9a9b
joonwan <joonwan1024@naver.com>
2025-03-23 23:01:16 +0900

    test: ClubRepository test code 작성

---
+++ src/test/java/com/ggs/ggspace/repository/club/ClubRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.ggs.ggspace.repository.club;
+
+import com.ggs.ggspace.domain.club.Club;
+import com.ggs.ggspace.repository.exception.common.FindByNullException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class ClubRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    ClubRepository clubRepository;
+
+    @DisplayName("동아리를 저장 및 조회 할 수 있다.")
+    @Test
+    void saveAndFind() {
+        //given
+        Club club = Club.builder()
+                .name("test club")
+                .description("this is test club")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        clubRepository.save(club);
+        em.clear();
+
+        //when
+        Club findClub = clubRepository.findById(club.getId());
+
+        //then
+        assertThat(findClub.getName()).isEqualTo(club.getName());
+        assertThat(findClub.getDescription()).isEqualTo(club.getDescription());
+    }
+
+    @DisplayName("동아리 조회시 null을 조회할 수 없다.")
+    @Test
+    void findByNull() {
+        assertThatThrownBy(() -> clubRepository.findById(null))
+                .isInstanceOf(FindByNullException.class);
+    }
+}
